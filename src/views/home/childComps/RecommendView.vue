9422dd5d0c01a5f0574ffafc47384110886f5f4a
luhy <1428537497@qq.com>
2021-12-22 18:45:31 +0800

    first

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-item" v-for="item in recommends" :key="item.index">
+      <a :href="item.link">
+      <img :src="item.image" alt="">
+      <div>{{item.title}}</div>
+      </a>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script scoped>
+<style>
+.recommend {
+  display: flex;
+  width: 100%;
+}
+.recommend-item {
+  flex: 1;
+  text-align: center;
+}
+.recommend-item img {
+  width: 6rem;
+  height: 6rem;
+  margin-bottom: 0.5rem;
+}
+</style>
